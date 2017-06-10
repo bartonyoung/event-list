d91081612ca954c455ea3b0b8a3937ad050a3e94
bartonyoung <barton.young1986@gmail.com>
2017-06-10 15:36:01 -0700

    Create GH repo, add axios, test basic endpoint.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super()
+
+    this.fetchEvents = this.fetchEvents.bind(this)
   }
+
+  fetchEvents () {
+    axios.get('https://testproject-api.strv.com/events')
+      .then(results => console.log('data', results))
+  } 
 
   render () {
     return (
-      <div>hello from Event List</div>
+      <div>
+        <div>hello from Event List</div>
+        <button onClick={ ()=> {this.fetchEvents()} }>EVENTS</button>
+      </div>  
     )
   }
 }
