4056e27765d1cd6705cf6fc56b3ee2ac647535f7
bartonyoung <barton.young1986@gmail.com>
2017-06-10 15:18:01 -0700

    Initial Event-list setup.

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  constructor (props) {
+    super()
+  }
+
+  render () {
+    return (
+      <div>hello from Event List</div>
+    )
+  }
+}
+
+export default App;
